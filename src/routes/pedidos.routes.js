841847aa75970ec41f395db6343d0899162f5c9c
AdonisMarlon <marlonavila467@gmail.com>
2026-06-30 14:15:03 -0500

    Implementacion de pedidos 3

--- src/routes/pedidos.routes.js
+++ src/routes/pedidos.routes.js
@@ -1,15 +1,10 @@
 import { Router } from 'express';
-import { getPedidos, getPedidosxid, postInsertarPedido , putPedido } from '../controladores/pedidosCtrl.js'
+import {postInsertarPedido} from '../controladores/pedidosCtrl.js'
 
 import { verificarToken } from '../middlewares/auth.js';
 
 const router = Router()
 
-router.get('/pedidos', verificarToken, getPedidos);
-router.get('/pedidos/:id', verificarToken, getPedidosxid); 
-
 router.post('/pedidos', verificarToken, postInsertarPedido);
-router.put('/pedidos/:id', verificarToken, putPedido);  
-
 
 export default router
